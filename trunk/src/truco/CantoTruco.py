9c69687b812c04836178946e46a0abb3b89e8d21
carbajo <carbajo@c1325aea-f840-0410-b9f2-2be20348b037>
2007-12-16 05:28:11 +0000

    Actualicé puntajes de los cantos del Truco/src/truco/CantoTruco.py

--- trunk/src/truco/CantoTruco.py
+++ trunk/src/truco/CantoTruco.py
@@ -37,8 +37,8 @@ class _cantotruco:
     return 'CantoTruco('+repr(self.codigo)+', '+repr(self.valor)+')'
 
 # pseudo constantes
-TRUCO = _cantotruco('TRUCO', 1)
-RETRUCO = _cantotruco('RETRUCO', 2)
+TRUCO = _cantotruco('TRUCO', 2)
+RETRUCO = _cantotruco('RETRUCO', 3)
 VALE4 = _cantotruco('VALE4', 4)
 QUIERO = _cantotruco('QUIERO', 0)
 NOQUIERO = _cantotruco('NOQUIERO', 0)
