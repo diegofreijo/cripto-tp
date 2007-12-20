9369036e82af682477448d9c0c95a2a5da358f33
carbajo <carbajo@c1325aea-f840-0410-b9f2-2be20348b037>
2007-12-20 18:19:57 +0000

    primos fuertes!

--- trunk/src/pruebas/primos.py
+++ trunk/src/pruebas/primos.py
@@ -9,3 +9,10 @@ print "Comenzando pruebas de " + str(cant) + " primos de " + str(bits) + " bits"
 for i in xrange(10):
 	tiempo = time.clock()
 	print str(i) + ": " + str(Azar.Primo(bits)) + "		->	" + str(time.clock() - tiempo) + " segundos"
+
+bits = 128
+cant = 10
+print "Comenzando pruebas de " + str(cant) + " primos fuertes de " + str(bits) + " bits"
+for i in xrange(10):
+	tiempo = time.clock()
+	print str(i) + ": " + str(Azar.PrimoFuerte(bits,10,10)) + "		->	" + str(time.clock() - tiempo) + " segundos"
