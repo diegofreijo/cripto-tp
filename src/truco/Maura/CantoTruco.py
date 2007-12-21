751c9c35098068f4734f51d32a79560fad5e4b1a
venturinimaura <venturinimaura@c1325aea-f840-0410-b9f2-2be20348b037>
2007-12-21 17:24:01 +0000

    Subo lo que hablamos ayer. Tengo problemas para comparar dos cantos. El error me lo tira en la funcion __eg__ de CantoEnvido.py. Si alguno me puede dar una mano mejor!. Tuve que hacer un objeto de Mano y otro de pie para poder jugar porque me hacia lo mismo de una lado y del otro

--- src/truco/Maura/CantoTruco.py
+++ src/truco/Maura/CantoTruco.py
@@ -3,7 +3,7 @@
 # Uso:
 # import CantoTruco
 
-class _cantotruco:
+class _cantoTruco:
   codigo = None
 
   def __init__(self, codigo):
@@ -15,7 +15,7 @@ class _cantotruco:
     return self.codigo
 
   def __eq__(self, otro):
-    if isinstance(otro, _cantotruco):
+    if isinstance(otro, _cantoTruco):
       return (self.codigo == otro.codigo)
     raise ValueError('Argumento de tipo no permitido: ' + str(otro))
 
@@ -32,8 +32,25 @@ class _cantotruco:
     return 'CantoTruco('+repr(self.codigo)+')'
 
 # pseudo constantes
-TRUCO = _cantotruco('TRUCO')
-RETRUCO = _cantotruco('RETRUCO')
-VALE4 = _cantotruco('VALE4')
-QUIEROTRUCO = _cantotruco('QUIERO')
-NOQUIEROTRUCO = _cantotruco('NO QUIERO')
+TRUCONOCANTADO=_cantoTruco('NADA')
+TRUCO = _cantoTruco('TRUCO')
+RETRUCO = _cantoTruco('RETRUCO')
+VALE4 = _cantoTruco('VALE4')
+QUIEROTRUCO = _cantoTruco('QUIERO')
+NOQUIEROTRUCO = _cantoTruco('NO QUIERO')
+
+CANTOS_TRUCO=[TRUCONOCANTADO,TRUCO,RETRUCO,VALE4]
+
+def cantoSiguiente(canto):
+  i = None
+  if canto == TRUCONOCANTADO:
+    i=1
+  elif canto ==TRUCO:
+    i = 2
+  elif canto == RETRUCO:
+    i = 3
+  elif canto == VALE4:
+    i = 4
+  else:
+    raise ValueError('No es un canto de truco: ' + repr(canto))
+  return CANTOS_TRUCO[i]
