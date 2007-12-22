f39721e16650de0313de0ef0fe03ee8cc5f592e7
carbajo <carbajo@c1325aea-f840-0410-b9f2-2be20348b037>
2007-12-22 05:17:16 +0000

    intente optimizar la generacion de primos fuertes pero no pude

--- src/lib/Matematica.py
+++ src/lib/Matematica.py
@@ -81,8 +81,8 @@ def int2bin(n):
 def PotenciaModular(a, b, n):
 	a2p = a % n
 	a = 1
-	while b > 0:
-		if b & 1 > 0:
+	while b != 0:
+		if (b & 1) != 0:
 		  a = (a * a2p) % n
 		b = b >> 1
 		a2p = (a2p * a2p) % n
