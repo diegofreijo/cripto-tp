bf95af6590848b365e02ed37ecde5b9d78aaca5a
carbajo <carbajo@c1325aea-f840-0410-b9f2-2be20348b037>
2007-12-24 21:00:59 +0000

    agregados a informe intente optimizar primos fuertes, pero sigue siendo horriblemente lento

--- trunk/src/pruebas/primos.py
+++ trunk/src/pruebas/primos.py
@@ -3,14 +3,14 @@ sys.path.append("..\\lib")
 import Azar
 import time
 
-bits = 128
+bits = 256
 cant = 10
 print "Comenzando pruebas de " + str(cant) + " primos de " + str(bits) + " bits"
 for i in xrange(10):
 	tiempo = time.clock()
 	print str(i) + ": " + str(Azar.Primo(bits)) + "		->	" + str(time.clock() - tiempo) + " segundos"
 
-bits = 128
+bits = 256
 cant = 10
 print "Comenzando pruebas de " + str(cant) + " primos fuertes de " + str(bits) + " bits"
 for i in xrange(10):
