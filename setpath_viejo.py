cc6b0f104b934fe3aec6e58754b7935c16399726
matias.dc.uba.ar <matias.dc.uba.ar@c1325aea-f840-0410-b9f2-2be20348b037>
2007-12-27 20:08:02 +0000

    Agregado setpath_viejo.py para poder testear al mismo tiempo la versión "vieja" de ManoTruco y las versiones experimentales que incluyen tantos.

--- setpath_viejo.py
+++ setpath_viejo.py
@@ -7,6 +7,7 @@ if not os.path.exists(base):
   print '(cambiar la variable "base" para que apunte a la copia local del repositorio cripto-tp)'
   print ''
   quit()
-for path in (base + r'\src\lib', base + r'\src\red', base + r'\src\truco', base + r'\src\truco\Maura'):
+#for path in (base + r'\src\lib', base + r'\src\red', base + r'\src\truco', base + r'\src\truco\Maura'):
+for path in (base + r'\src\lib', base + r'\src\red', base + r'\src\truco', base + r'\src\truco_v1'):
   if os.path.exists(path):
     sys.path.append(path)
