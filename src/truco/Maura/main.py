e2c43b5494b1c53d59c3a20459b066a5c1b02e38
venturinimaura <venturinimaura@c1325aea-f840-0410-b9f2-2be20348b037>
2007-12-29 02:30:23 +0000

    Ultimos cambios implementacion de irse al mazo chota. No como dice Matias

--- src/truco/Maura/main.py
+++ src/truco/Maura/main.py
@@ -5,6 +5,9 @@ sys.path.append("truco")
 import Palo
 from Carta import *
 import ManoTruco
+from Score import _Score
+
+Score=_Score()
 
 def mostrarLista(jugadas):
   i=0
@@ -13,82 +16,81 @@ def mostrarLista(jugadas):
     i=i+1
   return
 
-cartasMano = []
-cartasMano.append( Carta( 3,Palo.ESPADA ) )
-cartasMano.append( Carta( 2,Palo.ESPADA ) )
-cartasMano.append( Carta( 1,Palo.ESPADA ) )
-cartasPie = []
-cartasPie.append( Carta( 1,Palo.BASTO ) )
-cartasPie.append( Carta( 2,Palo.ORO ) )
-cartasPie.append( Carta( 3,Palo.ORO ) )
+while Score.partidoGanado()==0:
 
-TrucoMano = ManoTruco.ManoTruco( cartasMano, True )
-TrucoPie = ManoTruco.ManoTruco( cartasPie, False )
+  cartasMano = []
+  cartasMano.append( Carta( 3,Palo.ESPADA ) )
+  cartasMano.append( Carta( 2,Palo.ESPADA ) )
+  cartasMano.append( Carta( 1,Palo.ESPADA ) )
+  cartasPie = []
+  cartasPie.append( Carta( 1,Palo.BASTO ) )
+  cartasPie.append( Carta( 2,Palo.ORO ) )
+  cartasPie.append( Carta( 3,Palo.ORO ) )
 
-while TrucoMano.terminado()!=None and TrucoPie.terminado()!=None:
-  if TrucoMano.esMiTurno:
-    jugadas = TrucoMano.jugadasPosibles()
-    mostrarLista(jugadas)
-    print "Elija el numero de la opcion a jugar: (empezando en 0)"
-    opcionInt = int(raw_input("> "))
-    opcionJugada = jugadas[opcionInt]
-    print "Usted eligio jugar  ",
-    print str( opcionJugada )
-    TrucoMano.jugar( opcionJugada );
-    print "TrucoMano.juegomio " + str(TrucoMano.juegoMio)
-    print "TrucoMano.juegootro " + str(TrucoMano.juegoOtro)
-    print "TrucoMano.esMiTurno "  + str(TrucoMano.esMiTurno)
-    print "TrucoMano.estadoTruco " + str(TrucoMano.estadoTruco)
-    print "Trucopie.juegomio " + str(TrucoPie.juegoMio)
-    print "Trucopie.juegootro " + str(TrucoPie.juegoOtro)
-    print "Trucopie.esMiTurno " + str(TrucoPie.esMiTurno)
-    print "TrucoPie.estadoTruco " + str(TrucoPie.estadoTruco)
-    
-    TrucoPie.recibirJugada( opcionJugada );
-    print "TrucoMano.juegomio " + str(TrucoMano.juegoMio)
-    print "TrucoMano.juegootro " + str(TrucoMano.juegoOtro)
-    print "TrucoMano.esMiTurno "  + str(TrucoMano.esMiTurno)
-    print "TrucoMano.estadoTruco " + str(TrucoMano.estadoTruco)
-    print "Trucopie.juegomio " + str(TrucoPie.juegoMio)
-    print "Trucopie.juegootro " + str(TrucoPie.juegoOtro)
-    print "Trucopie.esMiTurno " + str(TrucoPie.esMiTurno)
-    print "TrucoPie.estadoTruco " + str(TrucoPie.estadoTruco)
-    opcionInt = raw_input("Presione una tecla para continuar...")
-  elif TrucoPie.esMiTurno:
-    jugadas = TrucoPie.jugadasPosibles()
-    mostrarLista(jugadas)
-    print "\nELIJA LA OPCION QUE JUEGA LA COMPU: (empezando en 0)"
-    opcionInt = int(raw_input("> "))
-    opcionJugada = jugadas[opcionInt]
-    print "La compu juega  ",
-    print str( opcionJugada )
-    TrucoPie.jugar( opcionJugada )
-    print "TrucoMano.juegomio " + str(TrucoMano.juegoMio)
-    print "TrucoMano.juegootro " + str(TrucoMano.juegoOtro)
-    print "TrucoMano.esMiTurno "  + str(TrucoMano.esMiTurno)
-    print "TrucoMano.estadoTruco " + str(TrucoMano.estadoTruco)
-    print "Trucopie.juegomio " + str(TrucoPie.juegoMio)
-    print "Trucopie.juegootro " + str(TrucoPie.juegoOtro)
-    print "Trucopie.esMiTurno " + str(TrucoPie.esMiTurno)
-    print "TrucoPie.estadoTruco " + str(TrucoPie.estadoTruco)
+  TrucoMano = ManoTruco.ManoTruco( cartasMano, True )
+  TrucoPie = ManoTruco.ManoTruco( cartasPie, False )
 
-    TrucoMano.recibirJugada( opcionJugada )
-    print "TrucoMano.juegomio " + str(TrucoMano.juegoMio)
-    print "TrucoMano.juegootro " + str(TrucoMano.juegoOtro)
-    print "TrucoMano.esMiTurno "  + str(TrucoMano.esMiTurno)
-    print "TrucoMano.estadoTruco " + str(TrucoMano.estadoTruco)
-    print "Trucopie.juegomio " + str(TrucoPie.juegoMio)
-    print "Trucopie.juegootro " + str(TrucoPie.juegoOtro)
-    print "Trucopie.esMiTurno " + str(TrucoPie.esMiTurno)
-    print "TrucoPie.estadoTruco " + str(TrucoPie.estadoTruco)
+  while TrucoMano.terminado()!=None or TrucoPie.terminado()!=None:
+    if TrucoMano.esMiTurno:
+      jugadas = TrucoMano.jugadasPosibles()
+      mostrarLista(jugadas)
+      print "Elija el numero de la opcion a jugar: (empezando en 0)"
+      opcionInt = int(raw_input("> "))
+      opcionJugada = jugadas[opcionInt]
+      print "Usted eligio jugar  ",
+      print str( opcionJugada )
+      TrucoMano.jugar( opcionJugada );
+      TrucoPie.recibirJugada( opcionJugada );
+      opcionInt = raw_input("Presione una tecla para continuar...")
+    elif TrucoPie.esMiTurno:
+      jugadas = TrucoPie.jugadasPosibles()
+      mostrarLista(jugadas)
+      print "\nELIJA LA OPCION QUE JUEGA LA COMPU: (empezando en 0)"
+      opcionInt = int(raw_input("> "))
+      opcionJugada = jugadas[opcionInt]
+      print "La compu juega  ",
+      print str( opcionJugada )
+      TrucoPie.jugar( opcionJugada )
+      TrucoMano.recibirJugada( opcionJugada )
+  
+  if TrucoMano.terminado()==None:
+    if TrucoMano.ganePartida==True:
+      print "La Mano gano la Partida!"
+    else:
+      print "El Pie gano la Partida!"
+  elif TrucoPie.terminado()==None:
+    if TrucoPie.ganePartida==False:
+      print "La Mano gano la Partida!"
+    else:
+      print "El Pie gano la Partida!"
 
-if TrucoMano.terminado()==None:
-  if TrucoMano.ganePartida==True:
-    print "La Mano gano la Partida!"
-  else:
-    print "El Pie gano la Partida!"
-elif TrucoPie.terminado()==None:
-  if TrucoPie.ganePartida==False:
-    print "La Mano gano la Partida!"
-  else:
-    print "El Pie gano la Partida!"
+  if TrucoMano.cartasEnvido[0]<TrucoMano.PtosEnvidoOtro and TrucoMano.estadoEnvido==QUIEROENVIDO:
+    verificar=(raw_intput("Desea pedirle a la otra parte que muestre sus cartas? (s/n)"))
+    verificar.lower()
+    if verificar=='s':
+      # mando el pedido de Mostrar las cartas y el otro le devuelve el mensaje con las cartas
+      jugada=["MOSTRAR TANTOS",[]]
+      TrucoMano.jugar(jugada)
+      jugada[1]=jugada[1]+TrucoMano.cartasEnvido[1]
+      TrucoPie.recibirJugada(jugada)
+  elif TrucoPie.cartasEnvido[0]<TrucoPie.PtosEnvidoOtro and TrucoPie.estadoEnvido==QUIEROENVIDO:
+    verificar=(raw_intput("Desea pedirle a la otra parte que muestre sus cartas? (s/n)"))
+    verificar.lower()
+    if verificar=='s':
+      # mando el pedido de Mostrar las cartas y el otro le devuelve el mensaje con las cartas
+      jugada=["MOSTRAR TANTOS",[]]
+      TrucoPie.jugar(jugada)
+      jugada[1]=jugada[1]+TrucoPie.cartasEnvido[1]
+      TrucoMano.recibirJugada(jugada)
+  # Aca muetsro el Score y juego una nueva Mano
+  print "El estado del Score es:\n"
+  mano=TrucoMano.ptosGanados()
+  pie=TrucoPie.ptosGanados()
+  print "TrucoMano.ptosGanados() " + str(mano)
+  print "TrucoPie.ptosGanados() " + str(pie)
+  Score.incrementarSocreMio(mano)
+  Score.incrementarScoreOtro(pie)
+  print str(Score)
+  
+print "\tRESULTADO FINAL DEL SCORE:"
+print str(Score)
