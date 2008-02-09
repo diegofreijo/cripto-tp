48b2c8e70ea07ea3fe793d582da2830615157d5d
venturinimaura <venturinimaura@c1325aea-f840-0410-b9f2-2be20348b037>
2008-02-09 19:46:48 +0000

    incorpore los archivos con algunas correcciones para emprolijar el codigo

--- src/truco/Carta.py
+++ src/truco/Carta.py
@@ -1,4 +1,3 @@
-# -*- coding: cp1252 -*-
 # TP
 # Matias
 # Uso:
@@ -8,63 +7,63 @@
 import Palo
 
 class Carta:
-  _palo = None
-  _numero = None
+  palo = None
+  numero = None
 
   def __init__(self, numero, palo):
     arg0 = numero
     arg1 = palo
-    if Palo.esPalo(arg0) and isinstance(arg1, int):
+    if isinstance(arg0, Palo._palo) and isinstance(arg1, int):
       palo = arg0
       numero = arg1
-    elif isinstance(arg0, int) and Palo.esPalo(arg1):
+    elif isinstance(arg0, int) and isinstance(arg1, Palo._palo):
       palo = arg1
       numero = arg0
     else:
       raise ValueError('Argumentos de tipo no permitido - deben ser int y Palo.')
     if numero < 1 or (numero >= 8 and numero <= 9) or numero > 12:
       raise ValueError('Numero de la carta incorrecto: ' + str(numero))
-    self._palo = palo
-    self._numero = numero
+    self.palo = palo
+    self.numero = numero
 
-  def palo(self):
-    return self._palo
+  def palo():
+    return self.palo
 
-  def numero(self):
-    return self._numero
+  def numero():
+    return self.numero
 
   def esBasto(self):
-    return self._palo.esBasto()
+    return self.palo.esBasto()
 
   def esCopa(self):
-    return self._palo.esCopa()
+    return self.palo.esCopa()
 
   def esEspada(self):
-    return self._palo.esEspada()
+    return self.palo.esEspada()
 
   def esOro(self):
-    return self._palo.esOro()
+    return self.palo.esOro()
 
   def esFigura(self):
-    return self._numero >= 10
+    return self.numero >= 10
 
   def __eq__(self, otra):
     if isinstance(otra, Carta):
-      return (self._palo == otra._palo and self._numero == otra._numero)
+      return (self.palo == otra.palo and self.numero == otra.numero)
     raise ValueError('Argumento de tipo no permitido: ' + str(otra))
 
   def __ne__(self, otra):
     return not (self == otra)
 
   def __hash__(self):
-    return self._palo.__hash__() ^ self._numero.__hash__()
+    return self.palo.__hash__() ^ self.numero.__hash__()
 
   def __str__(self):
-    sn = str(self._numero)
-    if (self._numero == 1): sn = 'Ancho'
-    if (self._numero == 12): sn = 'Rey'
-    return sn + ' de ' + str(self._palo)
+    sn = str(self.numero)
+    if (self.numero == 1): sn = 'Ancho'
+    if (self.numero == 12): sn = 'Rey'
+    return sn + ' de ' + str(self.palo)
 
   def __repr__(self):
-    return 'Carta('+repr(self._numero)+', '+repr(self._palo)+')'
+    return 'Carta('+repr(self.numero)+', '+repr(self.palo)+')'
 
